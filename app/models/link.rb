264222ea2a9208a7d703284a789f2a9513bf23cc
Justine <>
2018-02-01 19:01:15 +0100

    ok

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  VALID_LINK_REGEX = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+	validates :url, presence: true,
+			            format: { with: VALID_LINK_REGEX }
 end
